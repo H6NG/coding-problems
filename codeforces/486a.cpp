d5cbcc498a771ac710b316461074b17ad1fad57e
H6NG <8.h.a.n.g.l.i.u.8@gmail.com>
2026-07-29 18:50:47 -0400

    .

---
+++ codeforces/486a.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std; 
+/* answer that works, but O(n) 
+int main(){
+
+    long long n, m, sum = 0; 
+    cin >> n; 
+
+    for(int i = 1; i <= n; i++){
+
+        if(i%2 == 1) sum+= i * -1; 
+        else sum += i; 
+    }
+    cout << sum; 
+    return 0; 
+}
+*/
+
+// O(1) answer; 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long n;
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << n / 2;
+    else
+        cout << -(n + 1) / 2;
+
+    return 0;
+}
